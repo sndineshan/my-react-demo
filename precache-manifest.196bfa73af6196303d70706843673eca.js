7d2d214bcff104aaffae548d0c295d32c7f73314
Dineshan <sndineshan@gmail.com>
2019-03-02 17:35:13 +0530

    Updates

---
+++ precache-manifest.196bfa73af6196303d70706843673eca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "57203521a7314fb5311a",
+    "url": "/my-react-demo/static/js/runtime~main.57203521.js"
+  },
+  {
+    "revision": "3fc5930874dce4236176",
+    "url": "/my-react-demo/static/js/main.3fc59308.chunk.js"
+  },
+  {
+    "revision": "1b1b32001669ee7961b5",
+    "url": "/my-react-demo/static/js/2.1b1b3200.chunk.js"
+  },
+  {
+    "revision": "ebd2c4577606ac250a49e3d9ee188747",
+    "url": "/my-react-demo/index.html"
+  }
+];
